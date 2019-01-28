f9a2d484221e60c1ee6e72ceb9639380066eb67b
Josh <shouppejosh@gmail.com>
2019-01-28 17:41:17 -0500

    resume formatting and content

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-layout class="padding">
-      <v-flex xs12 sm6 offset-sm3>
+      <v-flex xs12 sm8 offset-sm2>
         <v-card flat>
           <v-card-title primary-title>
             <div>
@@ -9,50 +9,88 @@
               <div class="grey--text">Web Developer (React.js, Vue.js)</div>
             </div>
           </v-card-title>
-          <v-card-text>
-            <div>
-              <h3>Halo Platform, Remote - <i>Frontend Engineer</i></h3>
-              <div class="grey--text">Feb 2018 - Nov 2018</div>
-              <blockquote>Lead frontend development of centralized exchange (vue.js, vuex). 
-                Built admin portal and worked with admin REST APIs. Worked on decentralized 
-                exchange until launch. Using GraphQL created snapshots of tickers in node.js, 
-                adapted ticker data to OHLC for custom datafeed for TradingView chart. Customized 
-                and implemented charting. Using vuetify library, created frontend components for 
-                decentralized exchange. Worked with websockets for live datafeed. </blockquote>
-            </div>
-          </v-card-text>
-          <v-card-text>
-            <div>
-              <div><b>iGenius</b>, Milan, Italy - <i>Frontend Web Developer</i></div>
-              <div class="grey--text">Feb 2017 - Feb 2018</div>
-              <blockquote>Using react.js with redux, developed frontend components for virtual 
-                advisor to manage social channels and marketing strategies. Product called “Crystal”.</blockquote>
-            </div>
-          </v-card-text>
-          <v-card-text>
-            <div>
-              <h3>Accenture, Milan, Italy - <i>Application Developer</i></h3>
-              <div class="grey--text">Feb 2015 - June 2017</div>
-              <blockquote>Started out as intern due to language barrier, promoted to senior 
-                and given work visa once language and communication was sufficient. Developed 
-                frontend components consulting for companies such as Unicredit, Fiat, Jeep, Kitkat, 
-                Nestle, and Monte dei Paschi di Siena. JavaScript, REST, jQuery, SASS/LESS.</blockquote>
-            </div>
-          </v-card-text>
-          <v-card-text>
-            <div>
-              <h3>Zenzio, Tampa, FL - <i>Jr. Web Developer</i></h3>
-              <div class="grey--text">Sep 2014 - Feb 2015</div>
-              <blockquote>C#, MVC, HTML, CSS. Began career in frontend learning the basics.</blockquote>
-            </div>
-          </v-card-text>
-          <v-card-text>
-            <div>
-              <h3>University of South Florida, Tampa, FL - <i>Management Information Systems</i></h3>
-              <div class="grey--text">Sep 2009 - Sep 2014</div>
-              <blockquote>Bachelor of Science</blockquote>
-            </div>
-          </v-card-text>
+          <v-layout>
+            <v-flex xs9>
+              <v-card-text>
+                <div>
+                  <h3 class="blue--text">Experience</h3>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <div><b>Halo Platform</b>, Remote - <i>Frontend Engineer</i></div>
+                  <div class="grey--text">Feb 2018 - Nov 2018</div>
+                  <blockquote>Lead frontend development of centralized exchange (vue.js, vuex). 
+                    Built admin portal and worked with admin REST APIs. Worked on decentralized 
+                    exchange until launch. Using GraphQL created snapshots of tickers in node.js, 
+                    adapted ticker data to OHLC for custom datafeed for TradingView chart. Customized 
+                    and implemented charting. Using vuetify library, created frontend components for 
+                    decentralized exchange. Worked with websockets for live datafeed. </blockquote>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <div><b>iGenius</b>, Milan, Italy - <i>Frontend Web Developer</i></div>
+                  <div class="grey--text">Feb 2017 - Feb 2018</div>
+                  <blockquote>Using react.js with redux, developed frontend components for virtual 
+                    advisor to manage social channels and marketing strategies. Product called “Crystal”.</blockquote>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <div><b>Accenture</b>, Milan, Italy - <i>Application Developer</i></div>
+                  <div class="grey--text">Feb 2015 - June 2017</div>
+                  <blockquote>Started out as intern due to language barrier, promoted to senior 
+                    and given work visa once language and communication was sufficient. Developed 
+                    frontend components consulting for companies such as Unicredit, Fiat, Jeep, Kitkat, 
+                    Nestle, and Monte dei Paschi di Siena. JavaScript, REST, jQuery, SASS/LESS.</blockquote>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <div><b>Zenzio</b>, Tampa, FL - <i>Jr. Web Developer</i></div>
+                  <div class="grey--text">Sep 2014 - Feb 2015</div>
+                  <blockquote>C#, MVC, HTML, CSS. Began career in frontend learning the basics.</blockquote>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <h3 class="blue--text">Education</h3>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <div><b>University of South Florida</b>, Tampa, FL - <i>Management Information Systems</i></div>
+                  <div class="grey--text">Sep 2009 - Sep 2014</div>
+                  <blockquote>Bachelor of Science</blockquote>
+                </div>
+              </v-card-text>
+            </v-flex>
+            <v-flex xs3>
+              <v-card-text>
+                <div>
+                  <h3 class="blue--text">Skills</h3>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <blockquote>JavaScript, Vue.js, vuex, react.js, redux, node.js, 
+                    REST API’s, websockets, charting / TradingView, 
+                    HTML5, CSS/SASS/LESS, Bootstrap, Vuetify, Material Design</blockquote>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <h3 class="blue--text">OS's</h3>
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <div>
+                  <blockquote>Linux, MacOS, Windows</blockquote>
+                </div>
+              </v-card-text>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
